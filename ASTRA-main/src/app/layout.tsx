0a42b92f8df74422c07f0dbedbeb71f80e778e17
mahekrutuja <mahekrutuja@gmail.com>
2025-11-02 13:44:39 +0000

    conversational_AI

--- ASTRA-main/src/app/layout.tsx
+++ ASTRA-main/src/app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
     <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
       <body className={`${ibmPlexSans.variable} ${sourceSerif4.variable} font-sans`}>
         {children}
+        <p>We have this conversational AI from Eleven Labs which is using Gemini 2.5 Flash and can speak in English, Hindi, Tamil, German, French, and Swedish as per your input.</p>
+        <elevenlabs-convai agent-id="agent_2101k920sx33f68bwtwww65fktn0"></elevenlabs-convai>
+        <script src="https://unpkg.com/@elevenlabs/convai-widget-embed" async type="text/javascript"></script>
         <Toaster />
       </body>
     </html>
